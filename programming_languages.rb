13abfdbbc302f2c03a87de8546510b19af24f645
makafis <vinnyalfieri@gmail.com>
2015-06-11 16:47:32 -0400

    complete

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,25 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
-end
+new_hash = {}
+  languages.each do |key, value| #keys = :oo or :functional
+    value.each do |progkey, progval| #progkey = :ruby, progval = {}
+
+        progval.each do |type, tval|
+
+            if new_hash.has_key?(progkey)
+              new_hash[progkey][:style] << [key]
+
+
+            else
+              new_hash[progkey] = {}
+              new_hash[progkey][type] = tval
+              new_hash[progkey][:style] = [key]
+              
+            end
+        end
+new_hash[progkey][:style] =  new_hash[progkey][:style].flatten
+    end #end do
+  end #end do
+
+new_hash
+end #end def
